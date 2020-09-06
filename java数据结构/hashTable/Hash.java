1c3157b5ed64a5cb3e10f8bc4baef83ecc53c11d
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-09-06 00:16:01 -0500

    Update Hash.java

--- java数据结构/hashTable/Hash.java
+++ java数据结构/hashTable/Hash.java
@@ -3,27 +3,30 @@ package hashTable;
 import java.util.Scanner;
 
 public class Hash {
-	
+
 	public static void main(String[] args) {
-		
+
 		HashTable hashTable = new HashTable(7);
-		//菜单
+		// 菜单
 		String keyString = "";
 		Scanner scanner = new Scanner(System.in);
-		
+
 		while (true) {
 			System.out.println("add:添加雇员");
 			System.out.println("list:显示雇员");
 			System.out.println("find:查询雇员");
 			System.out.println("exit:退出程序");
+
 			keyString = scanner.nextLine();
-			
+
 			switch (keyString) {
-			case "add": 
+			case "add":
 				System.out.println("请输入id:");
 				int id = scanner.nextInt();
 				System.out.println("请输入名字:");
 				String nameString = scanner.next();
+
+				// 创建雇员
 				Emp emp = new Emp(id, nameString);
 				hashTable.add(emp);
 				break;
@@ -44,6 +47,4 @@ public class Hash {
 		}
 	}
 
-
 }
-
