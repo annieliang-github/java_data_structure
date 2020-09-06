87893c14e69e160f4d6df4f1149a13ad42397f01
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-09-05 23:59:00 -0500

    Add files via upload

---
+++ java数据结构/Emp.java
@@ -0,0 +1,14 @@
+package hashTable;
+
+public class Emp {
+	
+	public int id;
+	public String name;
+	public Emp next;
+	
+	public Emp(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}	
+}
