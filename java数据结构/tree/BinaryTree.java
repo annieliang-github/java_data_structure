2ab61f05b3b075de49fa552a72889eae000867c2
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-09-08 10:23:35 -0500

    Add files via upload

--- java数据结构/tree/BinaryTree.java
+++ java数据结构/tree/BinaryTree.java
@@ -2,4 +2,36 @@ package tree;
 
 public class BinaryTree {
 
+	private HeroNode rootHeroNode;
+
+	public void setRootHeroNode(HeroNode rootHeroNode) {
+		this.rootHeroNode = rootHeroNode;
+	}
+
+	// 调用前序
+	public void preOrder() {
+		if (this.rootHeroNode != null) {
+			this.rootHeroNode.preOrder();
+		} else {
+			System.out.println("二叉树为空，无法遍历");
+		}
+	}
+
+	// 调用中序
+	public void infixOrder() {
+		if (this.rootHeroNode != null) {
+			this.rootHeroNode.infixOrder();
+		} else {
+			System.out.println("二叉树为空，无法遍历");
+		}
+	}
+
+	// 调用后序
+	public void postOrder() {
+		if (this.rootHeroNode != null) {
+			this.rootHeroNode.postOrder();
+		} else {
+			System.out.println("二叉树为空，无法遍历");
+		}
+	}
 }
