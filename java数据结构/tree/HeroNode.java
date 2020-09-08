7b11547ab063df4482c5e3a48e83ad6a284576e2
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2020-09-08 10:18:59 -0500

    Update HeroNode.java

--- java数据结构/tree/HeroNode.java
+++ java数据结构/tree/HeroNode.java
@@ -2,4 +2,105 @@ package tree;
 
 public class HeroNode {
 
+	private int no;
+	private String nameString;
+	private HeroNode leftHeroNode;
+	private HeroNode riHeroNode;
+	
+	public HeroNode(int no, String nameString) {
+		super();
+		this.no = no;
+		this.nameString = nameString;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getNameString() {
+		return nameString;
+	}
+
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+
+	public HeroNode getLeftHeroNode() {
+		return leftHeroNode;
+	}
+
+	public void setLeftHeroNode(HeroNode leftHeroNode) {
+		this.leftHeroNode = leftHeroNode;
+	}
+
+	public HeroNode getRiHeroNode() {
+		return riHeroNode;
+	}
+
+	public void setRiHeroNode(HeroNode riHeroNode) {
+		this.riHeroNode = riHeroNode;
+	}
+	
+	//前序
+	public void preOrder() {
+		
+		System.out.println(this);
+		
+		if (this.leftHeroNode != null) {
+			this.leftHeroNode.preOrder();
+		}
+		
+		if (this.riHeroNode != null) {
+			this.riHeroNode.preOrder();
+		}
+	}
+	
+	//中序
+	public void infixOrder() {
+		
+		if (this.leftHeroNode != null) {
+			this.leftHeroNode.preOrder();
+		}
+		
+		System.out.println(this);
+		
+		if (this.riHeroNode != null) {
+			this.riHeroNode.preOrder();
+		}
+	}
+	
+	//后序
+	public void postOrder() {
+		
+		if (this.leftHeroNode != null) {
+			this.leftHeroNode.preOrder();
+		}
+	
+		if (this.riHeroNode != null) {
+			this.riHeroNode.preOrder();
+		}
+		
+		System.out.println(this);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
